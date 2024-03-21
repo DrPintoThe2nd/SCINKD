f516393c837485980e1eefc6d6f7d874b0ae7411
Brendan J. Pinto <18681591+DrPintoThe2nd@users.noreply.github.com>
2024-03-21 19:33:59 -0400

    Update scinkd_v0.1.sh
    removed accessory comments

--- scinkd_v0.1.sh
+++ scinkd_v0.1.sh
@@ -12,16 +12,6 @@ jellyfish dump $VARIABLE\.counts.jf | pigz -c --best > $VARIABLE\.k28.counts.dum
 rm $VARIABLE\.counts.jf
 done
 
-#HAP1
-#jellyfish count -m 28 -s 2000M -t $3 <(pigz -dc $1\.gz) -o $1\.counts.jf
-#jellyfish dump $1\.counts.jf | pigz -c --best > $1\.k28.counts.dumps.fa.gz
-#rm $1\.counts.jf
-#
-##HAP2
-#jellyfish count -m 28 -s 2000M -t $3 <(pigz -dc $2\.gz) -o $2\.counts.jf
-#jellyfish dump $2\.counts.jf | pigz -c --best > $2\.k28.counts.dumps.fa.gz
-#rm $2\.counts.jf
-
 #compare and contrast HAPs
 bbduk.sh -Xmx300g threads=4 in=$1\.k28.counts.dumps.fa.gz out=$1\_HAP1-only_28mers.fa.gz ref=$2\.k28.counts.dumps.fa.gz k=28 hdist=0 
 bbduk.sh -Xmx300g threads=4 in=$2\.k28.counts.dumps.fa.gz out=$2\_HAP2-only_28mers.fa.gz ref=$1\.k28.counts.dumps.fa.gz k=28 hdist=0 
