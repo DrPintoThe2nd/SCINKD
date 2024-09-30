ddbd79aad7849e85b9cb6249be1bda67fa35808c
Brendan J. Pinto <18681591+DrPintoThe2nd@users.noreply.github.com>
2024-09-30 12:59:42 -0700

    Add files via upload

--- scinkd_v0.2.sh
+++ scinkd_v0.2.sh
@@ -2,8 +2,8 @@
 
 #count kmers
 echo "counting kmers..."
-time meryl count k=28 memory=16 threads=12 output $1\.meryl $1
-time meryl count k=28 memory=16 threads=12 output $2\.meryl $2
+time meryl count k=28 memory=$3 threads=$4 output $1\.meryl $1
+time meryl count k=28 memory=$3 threads=$4 output $2\.meryl $2
 
 #negate kmers from each haploypes
 echo "negating kmers..."
@@ -30,4 +30,3 @@ do
 grep "$line" hap2-minus-hap1.bed | wc -l >> hap2-minus-hap1.out 
 done
 paste hap2-minus-hap1.txt hap2-minus-hap1.out 
-
